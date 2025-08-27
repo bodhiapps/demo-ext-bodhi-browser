67cdfc808eb994f3bae1c68585846cc831961854
anagri <127566+anagri@users.noreply.github.com>
2025-08-27 07:59:26 +0530

    [Amir] configured for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
+  basePath: '/demo-ext-bodhi-browser',
+  assetPrefix: '/demo-ext-bodhi-browser/',
 };
 
 export default nextConfig;
